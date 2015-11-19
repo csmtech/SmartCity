89aab6a4246028da7b57b765d7c4650bbde20e66
csmtech <csmtecch.web@gmail.com>
2015-11-19 11:42:25 +0530

    test by rasmikant

--- app/src/main/java/com/csm/smartcity/information/InformationActivity.java
+++ app/src/main/java/com/csm/smartcity/information/InformationActivity.java
@@ -43,7 +43,7 @@ public class InformationActivity extends AppCompatActivity {
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        /// Inflate the menu; this adds items to the action bar if it is present. 
+        /// Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_back, menu);
         return true;
     }
